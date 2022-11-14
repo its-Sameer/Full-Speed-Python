2dc083185e5744d4ff295033c2917f129adf2337
its-Sameer <117700788+its-Sameer@users.noreply.github.com>
2022-11-14 10:48:42 +0530

    Create solution.py

---
+++ Basic Data Types/solution.py
@@ -0,0 +1,13 @@
+def MathOP():
+  classic_division = 3 / 2
+  floor_division = 3 // 2
+  modulus = 3 % 2
+  power = 3 ** 2
+  
+  return[classic_division, floor_division, modulus, power]
+
+[classic_division, floor_division, modulus, power] = MathOP()
+print(classic_division)
+print(floor_division)
+print(modulus)
+print(power)
